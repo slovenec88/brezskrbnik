f6bfa6a84e71f9f7c14e424e016269dfd9140503
Davor <slovenec88@gmail.com>
2011-10-25 23:49:50 +0200

    Dodan splashscreen
    Change-Id: I7f4153a0eaa2a18883618d5fb925cc2529902c11

---
+++ src/edu/gricar/brezskrbnik/ActivitySplashScreen.java
@@ -0,0 +1,60 @@
+package edu.gricar.brezskrbnik;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+public class ActivitySplashScreen extends Activity{
+	//how long until we go to the next activity
+	protected int _splashTime = 1000; 
+
+	private Thread splashTread;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.splash);
+
+	    final ActivitySplashScreen sPlashScreen = this; 
+
+	    // thread for displaying the SplashScreen
+	    splashTread = new Thread() {
+	        @Override
+	        public void run() {
+	            try {
+	            	synchronized(this){
+
+	            		//wait 5 sec
+	            		wait(_splashTime);
+	            	}
+
+	            } catch(InterruptedException e) {}
+	            finally {
+	                finish();
+
+	                //start a new activity
+	                Intent i = new Intent();
+	                i.setClass(sPlashScreen, ActivityBrezskrbnik.class);
+	        		startActivity(i);
+
+	                stop();
+	            }
+	        }
+	    };
+
+	    splashTread.start();
+	}
+
+	//Function that will handle the touch
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+	    if (event.getAction() == MotionEvent.ACTION_DOWN) {
+	    	synchronized(splashTread){
+	    		splashTread.notifyAll();
+	    	}
+	    }
+	    return true;
+	}
+}
