1d5d558d8fd7158b7df8551e671ca0118af46ffc
Davor <slovenec88@gmail.com>
2011-11-30 22:46:48 +0100

    Dodan en spešl list, senzorji
    Change-Id: Ibf35286f5ebb288cfa663762fe338a5e19e5ddb8

--- src/edu/gricar/brezskrbnik/pomoc/ActivityPomoc.java
+++ src/edu/gricar/brezskrbnik/pomoc/ActivityPomoc.java
@@ -2,9 +2,12 @@ package edu.gricar.brezskrbnik.pomoc;
 
 import edu.gricar.brezskrbnik.R;
 import edu.gricar.brezskrbnik.R.layout;
+import edu.gricar.brezskrbnik.budilka.AlarmActivity;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
 import android.widget.Button;
 
 public class ActivityPomoc extends Activity{
@@ -14,4 +17,20 @@ public class ActivityPomoc extends Activity{
 	        setContentView(R.layout.pomoc);
 
 	    }
+	   
+	   public void onKnof(View v) {
+
+			Intent i = new Intent(this.getApplicationContext(), ActivitySenzor.class);
+	    	startActivity(i);
+
+		}
+	   
+	   public void onKnofList(View v) {
+
+			Intent i = new Intent(this.getApplicationContext(), List11.class);
+	    	startActivity(i);
+
+		}
+	   
+	   
 }
---
+++ src/edu/gricar/brezskrbnik/pomoc/ActivitySenzor.java
@@ -0,0 +1,117 @@
+package edu.gricar.brezskrbnik.pomoc;
+
+import edu.gricar.brezskrbnik.R;
+import edu.gricar.brezskrbnik.R.layout;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.view.View;
+import android.view.animation.*;
+
+
+public class ActivitySenzor extends GraphicsActivity{   
+	public static SensorManager sSensorManager;
+	private static AnimateDrawable mDrawable;
+	private static Animation an;
+	public static float cz = 0;
+	public static float cx = 0;
+	public static float cy = 0;
+	public static int levo = 90;
+	public static int gor = 200;
+	public static int zzz = 0;
+	public static Drawable dr;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(new SampleView(this));
+		sSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		sSensorManager.registerListener(sSensorListener, sSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION | Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME);
+	}
+
+	@Override
+	protected void onStop() {
+		sSensorManager.unregisterListener(sSensorListener);
+		super.onStop();
+	}
+
+	private SensorEventListener sSensorListener = new SensorEventListener() {
+
+
+		public void onSensorChanged(SensorEvent event) {
+			cz = event.values[0];
+			cx = event.values[1];
+			cy = event.values[2];
+
+			if (cy > 10){
+				levo = 0;
+				startActivity(getIntent()); finish();
+
+			}
+
+
+			if (cy < - 10){
+				levo = 200;
+				startActivity(getIntent()); finish();
+
+			}
+
+			if (cx > 10){
+				gor = 0;
+				startActivity(getIntent()); finish();
+			}
+
+			if (cx < - 10){
+				gor = 300;
+				startActivity(getIntent()); finish();			
+			}
+		}
+
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		}
+	};
+
+	private static class SampleView extends View {
+		public SampleView(Context context) {
+			super(context);
+			setFocusable(true);
+			setFocusableInTouchMode(true);
+
+			dr = context.getResources().getDrawable(R.drawable.budilka);
+			dr.setBounds(0, 0, dr.getIntrinsicWidth(), dr.getIntrinsicHeight());
+			an = new TranslateAnimation(90, levo, 200, gor);
+			an.setDuration(2000);
+			an.setRepeatCount(1);
+			an.initialize(90, 90, 200, 200);
+
+			mDrawable = new AnimateDrawable(dr, an);
+			an.startNow();
+			levo = 90;
+			gor = 200;            
+		}
+
+
+		@Override protected void onDraw(Canvas canvas) {
+			canvas.drawColor(Color.BLACK);
+
+			mDrawable.draw(canvas);
+			invalidate();            
+		}
+	}
+}
