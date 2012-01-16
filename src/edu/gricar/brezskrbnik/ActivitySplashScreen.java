03b7247cbbef34b983c678e21459045dea766304
Davor <slovenec88@gmail.com>
2012-01-16 20:43:55 +0100

    Splah je v novi metodi, aplikacija se shrani na sd
    Change-Id: I9f8889565236cc1929f5a5059ef2ff9072a761e8

--- src/edu/gricar/brezskrbnik/ActivitySplashScreen.java
+++ src/edu/gricar/brezskrbnik/ActivitySplashScreen.java
@@ -1,13 +1,24 @@
 package edu.gricar.brezskrbnik;
 
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+
+import edu.gricar.brezskrbnik.budilka.AlarmActivity;
+import edu.gricar.brezskrbnik.budilka.AlarmActivity.BackgroundAsyncTask2;
+import edu.gricar.brezskrbnik.pomoc.ActivityPomoc;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.Window;
+import android.widget.Toast;
 
 public class ActivitySplashScreen extends Activity{
 	//how long until we go to the next activity
+    ApplicationBrezskrbnik app;
 	protected int _splashTime = 2000; 
 
 	private Thread splashTread;
@@ -19,11 +30,13 @@ public class ActivitySplashScreen extends Activity{
 	    
 	    requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 	    setContentView(R.layout.splash);
+	    app = (ApplicationBrezskrbnik) getApplication();
 	    setProgressBarIndeterminateVisibility(true);
 	    final ActivitySplashScreen sPlashScreen = this; 
-
+        BackgroundAsyncTask mt = new BackgroundAsyncTask();
+        mt.execute();
 	    // thread for displaying the SplashScreen
-	    splashTread = new Thread() {
+	    /*splashTread = new Thread() {
 	        @Override
 	        public void run() {
 	            try {
@@ -49,6 +62,8 @@ public class ActivitySplashScreen extends Activity{
 
 	    splashTread.start();
 	}
+	
+	
 
 	//Function that will handle the touch
 	@Override
@@ -59,5 +74,43 @@ public class ActivitySplashScreen extends Activity{
 	    	}
 	    }
 	    return true;
+	}*/
 	}
-}
+	
+	public class BackgroundAsyncTask extends AsyncTask<Void, Integer, String> {
+
+	        @Override
+	        protected String doInBackground(Void... params) {
+
+                    try {
+                        Thread.sleep(_splashTime);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+  
+
+                
+	            
+                return null;
+	        }
+
+	        protected void onPreExecute() {
+	            setProgressBarIndeterminateVisibility(true);
+	        }
+
+	        protected void onPostExecute(String arg) {
+	            setProgressBarIndeterminateVisibility(false);
+	            
+	            Intent i = new Intent(getApplicationContext(), ActivityBrezskrbnik.class);
+                startActivity(i);
+                finish();
+	           
+	        }
+	    }
+
+	   
+	    }
+	
+	
+
